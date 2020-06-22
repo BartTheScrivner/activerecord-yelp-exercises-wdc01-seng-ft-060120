c8604ab3df997aa0c3fa756f0a4551b2fc8ed8c4
BartTheScrivner <dframe22@gmail.com>
2020-06-22 18:08:18 -0400

    restaurant methods working through .focused

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,26 @@
 class Restaurant < ActiveRecord::Base 
   has_many :dishes
+
+  def self.mcdonalds
+    self.all.find_by(name: "McDonalds")
+  end
+
+  def self.tenth
+    self.all.find(10)
+  end
+
+  def self.with_long_names
+    pp self.all.where("length(name) > 12")
+  end
+
+  def self.max_dishes
+    self.all.joins(:dishes).group(:restaurant_id).order(Arel.sql("COUNT(restaurant_id) DESC")).first
+  end
+
+  def self.focused
+    pp self.all.joins(:dishes).group(:restaurant_id).having("COUNT(restaurant_id) < 5")
+  end
   
 end 
+
+
